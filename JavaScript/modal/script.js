3a2dd0659394466809b7828330d777698ff21aa1
Caro Oviedo <60893793+caro-oviedo@users.noreply.github.com>
2021-10-04 07:15:23 -0300

    Create script.js

---
+++ JavaScript/modal/script.js
@@ -0,0 +1,7 @@
+document.getElementById("open-modal").addEventListener("click", function() {
+  document.getElementById("overlay").style.display = "block";
+})
+
+document.getElementById("close-modal").addEventListener("click", function() {
+  document.getElementById("overlay").style.display = "none";
+})
